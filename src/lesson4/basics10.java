3515dd43e8dbf50ef8ad9c4835cd374c92fd65f9
Henry <hlin117@gmail.com>
2014-01-19 17:46:12 -0600

    Function exercises

--- src/lesson4/basics10.java
+++ src/lesson4/basics10.java
@@ -6,8 +6,56 @@ public class basics10 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Initializing a random array of length 20
+		int size = 20;
+		int[] array = new int[size];
+		randomArray(array, 0, 20);
 
+		firstLastSwap(array);
+		printArray(array);
+	}
+	
+	/**
+	 * Fills an array with random numbers between min and max.
+	 * 
+	 * You do not need to modify this function. 
+	 * 
+	 * @param array
+	 * @param min
+	 * @param max
+	 */
+	public static void randomArray(int[] array, int min, int max) {
+		for(int i = 0; i < array.length; i++) {
+			array[i] = (int) (Math.random() * (max - min)) + min;
+		}
+	}
+	
+	/**
+	 * TODO: Swap the first value and last value of the input array.
+	 * For example, if the input array were [9, 20, 0], the end result
+	 * of the array would be [0, 20, 9].
+	 * 
+	 * @param array
+	 */
+	public static void firstLastSwap(int[] array) {
+		
+		
+	
+	}
+	
+	/**
+	 * TODO: Print the contents of your array. in the following
+	 * format:
+	 * 
+	 * Index i: array[i]
+	 * 
+	 * @param array
+	 */
+	public static void printArray(int[] array) {
+		
+		
+		
 	}
 
 }
