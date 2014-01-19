0caebc2f41df9086b2388e123afec161c0b2ee2f
Henry <hlin117@gmail.com>
2014-01-19 16:44:07 -0600

    made exercises for arrays

--- src/lesson4/basics9.java
+++ src/lesson4/basics9.java
@@ -5,10 +5,28 @@ public class basics9 {
 	/**
 	 * Introduction to arrays
 	 * 
-	 * 
 	 */
 	public static void main(String[] args) {
-		int[] array = new int[10];
+		
+		// Creates an array of size 10, and 
+		int size = 10;
+		int[] array = new int[size];
+		for(int i = 0; i < 20; i++) {
+			array[i] = (int) (20 * Math.random()) + 1; // sets a number between 1 and 20 into array[i]
+		}
+		
+		/**
+		 * TODO: Iterate through the int[] array. 
+		 * If array[i] is even, print the line 
+		 * 
+		 * "index i is even and has value array[i]"
+		 * 
+		 * where i is the index number, and array[i] is the array's value at that index.
+		 * 
+		 * 
+		 * Also print out the total number of even numbers inside the array.
+		 */
+		
 		
 
 	}
