c7caa5812bd9d85dde7dcc38fdaacdcc7b79a623
Henry <hlin117@gmail.com>
2014-01-20 01:12:59 -0600

    hello world

---
+++ src/lesson4/basics11.java
@@ -0,0 +1,18 @@
+package lesson4;
+
+public class basics11 {
+
+	/**
+	 * The Mini math library.
+	 * 
+	 * This math library allows users to perform simple manipulations on
+	 * their inputted numbers. Please complete the functions below.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
--- src/lesson4/functions_example2.java
+++ src/lesson4/functions_example2.java
@@ -17,5 +17,22 @@ public class functions_example2 {
 		return i % 2 == 0;
 	}
 	
+	// Checks whether an array has a given number.
+	public static boolean has(int[] array, int num) {
+		for(int i = 0; i < array.length; i++)
+			if(array[i] == num) return true;
+		
+		return false;
+		
+	}
+	
+	// Returns an array of some size with a min and max value
+	public static int[] makeArray(int size, int min, int max) {
+		int[] array = new int[size];
+		for(int i = 0; i < array.length; i++)
+			array[i] = (int) ((max-min) * Math.random()) + min;
+		return array;
+	
+	}
 
 }
